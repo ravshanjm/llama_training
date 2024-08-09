e172e003d703e598b261af28e13d2ad15bd95113
ravshanjm <ravshanmuzaffar@gmail.com>
2024-08-09 16:13:20 +0500

    Update train.py

--- train.py
+++ train.py
@@ -1,5 +1,4 @@
 from datasets import load_dataset
-
 dataset = load_dataset("aisha-org/orpo_dataset_v1")
 
 import gc
@@ -8,6 +7,7 @@ import os
 import torch
 import wandb
 from datasets import load_dataset
+from google.colab import userdata
 from peft import LoraConfig, PeftModel, prepare_model_for_kbit_training
 from transformers import (
     AutoModelForCausalLM,
@@ -17,6 +17,13 @@ from transformers import (
     pipeline,
 )
 from trl import ORPOConfig, ORPOTrainer, setup_chat_format
+from accelerate import Accelerator
+
+
+
+wandb.login(key='190ea355e042b66c717ec2994563d1e8cf420446')
+
+
 
 attn_implementation = "flash_attention_2"
 torch_dtype = torch.bfloat16
@@ -64,41 +71,47 @@ def format_chat_template(row):
     row["rejected"] = tokenizer.apply_chat_template(row["rejected"], tokenize=False)
     return row
 
-dataset = dataset.map(
-    format_chat_template,
-    num_proc= os.cpu_count(),
-)
 
-dataset = dataset.train_test_split(test_size=0.02)
+def main():
+    dataset = dataset.map(
+        format_chat_template,
+        num_proc= os.cpu_count(),
+    )
+
+    dataset = dataset.train_test_split(test_size=0.02)
 
 
-orpo_args = ORPOConfig(
-    learning_rate=8e-6,
-    beta=0.1,
-    lr_scheduler_type="linear",
-    max_length=1024,
-    max_prompt_length=1024,
-    per_device_train_batch_size=16,
-    per_device_eval_batch_size=16,
-    gradient_accumulation_steps=4,
-    optim="paged_adamw_8bit",
-    num_train_epochs=2,
-    eval_strategy="steps",
-    eval_steps=0.2,
-    logging_steps=1,
-    warmup_steps=10,
-    report_to="wandb",
-    output_dir="./results",
-)
+    orpo_args = ORPOConfig(
+        learning_rate=9e-6,
+        beta=0.1,
+        lr_scheduler_type="linear",
+        max_length=1024,
+        max_prompt_length=1024,
+        per_device_train_batch_size=2,
+        per_device_eval_batch_size=2,
+        gradient_accumulation_steps=4,
+        optim="paged_adamw_8bit",
+        num_train_epochs=1,
+        evaluation_strategy="steps",
+        eval_steps=0.2,
+        logging_steps=1,
+        warmup_steps=10,
+        report_to="wandb",
+        output_dir="./results",
+    )
 
-trainer = ORPOTrainer(
-    model=model,
-    args=orpo_args,
-    train_dataset=dataset["train"],
-    eval_dataset=dataset["test"],
-    peft_config=peft_config,
-    tokenizer=tokenizer,
-)
-trainer.train()
-trainer.save_model(new_model)
+    trainer = ORPOTrainer(
+        model=model,
+        args=orpo_args,
+        train_dataset=dataset["train"],
+        eval_dataset=dataset["test"],
+        peft_config=peft_config,
+        tokenizer=tokenizer,
+    )
+    trainer.train()
+    trainer.save_model(new_model)
 
+
+
+if __name__ == "__main__":
+    main()
